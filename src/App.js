b40d2f3e345dbbd63a1075bb36b406a650aca918
belghiti-brahim <belghiti.brahim@pm.me>
2022-06-15 10:02:50 +0100

    changing_BrowserRouter_to_HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { HashRouter as Router, Switch, Route } from "react-router-dom"
 import './index.css'
 import {
     Home, About, FAQ, Error, Character
